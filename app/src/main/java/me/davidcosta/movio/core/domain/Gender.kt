3533ea77f62c6a8156005c830837fc1df2098231
davidpcosta <davidpcosta@gmail.com>
2025-04-22 01:53:53 -0300

    Implement gender labels.

--- app/src/main/java/me/davidcosta/movio/core/domain/Gender.kt
+++ app/src/main/java/me/davidcosta/movio/core/domain/Gender.kt
@@ -7,28 +7,28 @@ import me.davidcosta.movio.R
 import me.davidcosta.movio.core.api.model.person.GenderModel
 
 enum class Gender {
-    FEMALE,
     MALE,
+    FEMALE,
     NON_BINARY,
     UNSPECIFIED
 }
 
 fun GenderModel.toGender(): Gender =
     when (this) {
-        GenderModel.UNSPECIFIED -> Gender.UNSPECIFIED
-        GenderModel.FEMALE -> Gender.FEMALE
         GenderModel.MALE -> Gender.MALE
+        GenderModel.FEMALE -> Gender.FEMALE
         GenderModel.NON_BINARY -> Gender.NON_BINARY
+        GenderModel.UNSPECIFIED -> Gender.UNSPECIFIED
     }
 
 @Composable
 @ReadOnlyComposable
 fun Gender.label(): String =
     when (this) {
-        Gender.UNSPECIFIED -> stringResource(R.string.gender_label_unspecified)
-        Gender.FEMALE ->stringResource(R.string.gender_label_female)
         Gender.MALE ->stringResource(R.string.gender_label_male)
+        Gender.FEMALE ->stringResource(R.string.gender_label_female)
         Gender.NON_BINARY -> stringResource(R.string.gender_label_non_binary)
+        Gender.UNSPECIFIED -> stringResource(R.string.gender_label_unspecified)
     }
 
 
@@ -36,18 +36,18 @@ fun Gender.label(): String =
 @ReadOnlyComposable
 fun Gender.labelKnownAs(): String =
     when (this) {
-        Gender.UNSPECIFIED -> stringResource(R.string.person_details_screen_tab_biography_label_known_as_male)
-        Gender.FEMALE ->stringResource(R.string.person_details_screen_tab_biography_label_known_as_female)
         Gender.MALE ->stringResource(R.string.person_details_screen_tab_biography_label_known_as_male)
+        Gender.FEMALE ->stringResource(R.string.person_details_screen_tab_biography_label_known_as_female)
         Gender.NON_BINARY -> stringResource(R.string.person_details_screen_tab_biography_label_known_as_non_binary)
+        Gender.UNSPECIFIED -> stringResource(R.string.person_details_screen_tab_biography_label_known_as_male)
     }
 
 @Composable
 @ReadOnlyComposable
 fun Gender.labelAlsoKnownAs(): String =
     when (this) {
-        Gender.UNSPECIFIED -> stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_male)
-        Gender.FEMALE ->stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_female)
         Gender.MALE ->stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_male)
+        Gender.FEMALE ->stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_female)
         Gender.NON_BINARY -> stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_non_binary)
+        Gender.UNSPECIFIED -> stringResource(R.string.person_details_screen_tab_biography_label_also_known_as_male)
     }
